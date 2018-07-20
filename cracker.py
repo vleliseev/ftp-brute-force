e5f5249cd3702afd614338b5caa79e435a31b1e8
Vlad Eliseev <shmel3@openmailbox.org>
2018-07-20 23:48:22 +0300

    argparse, refactoring
    * Removed target class, added RequestPattern; 
    * Added ability to specify whole http request

--- cracker.py
+++ cracker.py
@@ -13,38 +13,27 @@
 ##                                                                                ##
 ## ============================================================================== ##
 
-
 ### TODO ###
-# * read query string
-# * let user set whole http request without parsing html
-# * rewrite commands
-# * add argparse
+# * read query string - done
+# * let user set whole http request without parsing html - done
+# * rewrite commands - done (commands removed)
+# * add argparse - done
+# * add colors
 ### END TODO ###
 
 
-import os
-import platform
 
-
-
-### local modules ###
-import htmlparser as hp
+### IMPORT MODULES ###
+from urllib.parse import urlparse, parse_qs
 import bruteforce as bf
-### end local modules ###
+import argparse
+import requests
+### END MODULES ###
 
 
 
 
-### aplication variables ###
-target_url = str()
-login_dict = str()
-logins = []
-password_dict = str()
-passwords = []
-### end definition ###
-
-
-# ================================= TARGET CLASS ================================= #
+# ================================= REQUEST_PATTERN CLASS ================================= #
 """
 Class 'target' contains main information about html code of target page:
 	* all html forms (<form method=...> ... </form>) found in code;
@@ -54,57 +43,18 @@ Class 'target' contains main information about html code of target page:
 	* pass_var - the same as login_var, but holds password user input
 """
 	
-class target:
-	def __init__(self, html_code):
-		self.html_forms = hp.get_forms(html_code)
-		self.login_form_id = hp.guess_login_form(self.html_forms)
+class RequestPattern:
+	def __init__(self):
 		self.url = str()
-		if self.login_form_id != -1:
-			self.login_form = self.html_forms[self.login_form_id]
-			self.login_var = hp.guess_login_variable(self.get_login_form_variables())
-			self.pass_var = hp.guess_pass_variable(self.get_login_form_variables())
-		self.forms_info = { }
+		self.cookies = { }
 		self.request_body = { }
-		self.unsuccess_sign = str() # look ask_unsuccess_sign() function
-
-
-	def set_login_form(self, login_form_id):
-		if self.login_form_id == -1:
-			return None
-		self.login_form = self.html_forms[login_form_id]
-		self.login_var = hp.guess_login_variable(self.get_login_form_variables())
-		self.pass_var = hp.guess_pass_variable(self.get_login_form_variables())
-
-
-	# returns input variables of login form
-	def get_login_form_variables(self):
-		if self.login_form_id == -1:
-			return None
-		login_form_data = hp.inspect_form(self.login_form)
-		return login_form_data['Variables']
+		self.headers = { }
+		self.query = { }
+		self.unsuccess_sign = str()
+		self.login_var = str()
+		self.pass_var = str()
+		self.http_method = str()
 	
-
-	# return all important information about login form
-	# e.g. method type, class name, input variables, etc.
-	def get_login_form_data(self):
-		if self.login_form_id == -1:
-			return None
-		login_form_data = hp.inspect_form(self.login_form)
-		return login_form_data
-
-	def generate_request_body(self):
-		"""
-		 Function generates dictionary of input variables 
-		 and its values (login form)
-		"""
-		input_variables = self.get_login_form_variables()
-		for var in input_variables:
-			value = input_variables[var][1]
-			if value == 'none':
-				value = ''
-			self.request_body[var] = value
-
-
 # ================================= CLASS END ================================= #		
 
 
@@ -114,69 +64,72 @@ class target:
 
 
 
-# ================================= MAIN FUNCTION ================================= #
+# ================================= MAIN FUNCTIONS ================================= #
+def define_args():
+	parser = argparse.ArgumentParser(prog='cracker')
 
-def main():
-	global logins
-	global passwords
+	login_group = parser.add_mutually_exclusive_group(required = True)
+	login_group.add_argument('-l', metavar = 'LOGIN', help = 'set attack login')
+	login_group.add_argument('-L', metavar = 'dict.txt', help = 'set login dictionary')
 
-	print('Http(s) cracker started. Type \'help\' for more information.')
-	while(True):
-		command = input("CRACKER > ")
-		if command == 'help':
-			print_help()
-	
-		elif command == 'exit':
-			exit(0)
-	
-		elif command == 'clear':
-			clear()
-	
-		elif command.split(' ')[0] == 'set':
-			if len(command.split(' ')) < 3:
-				print('Unknown command. Type \'help\' to get command list.')
-				continue
-			subcommand = command.split(' ')[1]
-			arg = command.split(' ')[2]
-			if not arg:
-				print('Error. Argument is empty.')
-			if subcommand == 'target':
-				target_url = arg
-			if subcommand == 'login':
-				logins.append(arg)
-			if subcommand == 'password':
-				passwords.append(arg)
-			if subcommand == 'login_list':
-				login_dict = open(arg, 'r')
-				logins = [login.replace('\n', '') for login in login_dict]
-			if subcommand == 'password_list':
-				password_dict = open(arg, 'r')
-				passwords = [passwd.replace('\n', '') for passwd in password_dict]
-	
-		elif command == 'inspect':
-			if target == '': 
-				print('Error: target url is not specified.');
-				continue
-			resp = bf.make_empty_get_request(target_url)
-			if resp == None:
-				continues	
-			inspect_page(resp.text)
-	
-		elif command == 'start':
-			if target == '': 
-				print('Error: target url is not specified.');
-				continue
-			if not logins:
-				print('Set login / login dictionary first.')
-				continue
-			if not passwords:
-				print('Set password / password dictionary first.')
-				continue
-			start(target_url)
-		else:
-			print('Unknown command. Type \'help\' to get command list.')
+	password_group = parser.add_mutually_exclusive_group(required = True)
+	password_group.add_argument('-p', metavar = 'PASSWORD', help = 'set attack password')
+	password_group.add_argument('-P', metavar = 'dict.txt', help = 'set password dictionary')
 
-# ================================= END OF FUNCTION ================================= #	
+	parser.add_argument('-m', metavar = "HTTP_METHOD", required = True, help = 'set http request method (POST, GET, etc.)')
+
+	parser.add_argument('-t', metavar = 'THREADS', type = int , default = 3, help = 'set number of threads to use (default: 3)')
+	parser.add_argument('target', help = 'set target host (specify login action if it exists) e.g. https://target.com/login.php')
+	parser.add_argument('--reverse', '-r', action = 'store_false', help = 'set brute force attack type')
+	return parser
+
+
+
+def main(args):
+	logins, passwords = [], []
+	login = args['l']
+	login_dict_path = args['L']
+	password = args['p']
+	password_dict_path = args['P']
+	num_threads = 3 
+	url = args['target']
+	method = args['m']
+	
+	# set brute force login(s) #
+	if login_dict_path:
+		login_dict = open(login_dict_path, 'r')
+		logins = [login.replace('\n', '') for login in login_dict]
+	elif login:
+		logins.append(login)
+	
+	# set brute force password(s) #
+	if password_dict_path:
+		password_dict = open(password_dict_path, 'r')
+		passwords = [passwd.replace('\n', '') for passwd in password_dict]
+	elif password:
+		passwords.append(password)
+
+	# specify number of threads to use #
+	if args['t']:
+		num_threads = args['t']
+	if login and password:
+		num_threads = 1
+
+
+	print('Starting brute force attack.')
+	request_pattern = generate_request_pattern()
+	request_pattern.url = url + request_pattern.query
+	request_pattern.http_method = method
+	print('Getting Cookie...')
+	request_pattern.cookies = requests.get(request_pattern.url).cookies
+	print('Done.')
+	
+	# brute force attack #
+	bf.brute_force(request_pattern, logins, passwords, num_threads)
+	
+	
+
+# ================================= END OF FUNCTIONS ================================= #	
 
 
 
@@ -187,139 +140,63 @@ def main():
 
 # ================================= APPLICATION COMMANDS ================================= #
 
-def print_help():
-	help = """
-List of commands:
-help - get help
-clear - clear application output
-set login [login] - set auth login
-set login_list [path/to/dict.txt] - set login dictionary (.txt)
-set password [password] - set auth password
-set password_list [path/to/dict.txt] - set password dictionaty (.txt)
-set target [url] - set attack target (url or ip)
-start - start attack
-inspect - get information about page forms that contain http requests
-exit - close http cracker
+def ask_http_headers():
+	print('Set http headers: ')
+	headers = { }
+	header = input()
+	while header:
+		key = header.split(':')[0]
+		value = header.split(':')[1]
+		headers[key] = value
+		header = input()
+	return headers
 
-* unsuccess sign is string that is stored in html code of page
-  if auth request was unsuccessful.
-  e.g. "Failed login attemp.", "Error. Try again.", etc.
-"""
-	print(help)
 
+def ask_query_string():
+	"""
+	Returns query string e.g. ?var1=value1&var2=value2
+	"""
+	print('Set query string: ')
+	query = "?" 
+	param = input()
+	if param.count('&') > 0 and param.count('=') > 1:
+		return query + param
 	
-def clear():
-	sys_name = platform.system()
-	if sys_name == 'Windows':
-		os.system('cls')
-	elif sys_name == 'Linux' or sys_name == 'Unix':
-		os.system('clear')
-
-
-
-def inspect_page(html_code):
-	"""
-	This function parses all html_code, finds html forms there (<form method=...> ... </form>), 
-	its input variables, method types, etc. Function also provides (standart output) user with 
-	parsed information and return object of 'target' class that contains all parsed data.
-	"""
-	def convert_for_output(value):
-		return '\'' + value + '\''
-	print('Parsing html...\n')
-	t = target(html_code)
-	for i in range(len(t.html_forms)):
-		form_data = hp.inspect_form(t.html_forms[i])
-		t.forms_info[t.html_forms[i]] = form_data
-		print('Form number: ' + str(i))
-		print('Method type: ', form_data['Method'])
-		print('Form action: ', form_data['Action'])
-		print('Form class: ', form_data['Class'])
-		print('Form variables:')
-		variables = form_data['Variables']
-		for name in variables:
-			var_type = variables[name][0]
-			value = variables[name][1]
-			if value != 'none':
-				value = convert_for_output(value)
-			if var_type != 'none':
-				var_type = convert_for_output(var_type)
-			name = convert_for_output(name)
-			print('\tVariable ' + name + ' has', end = ' ')
-			print(var_type + ' input type and ' + value + ' value.')
-	print('Done.\n')
-	return t
-
-
-
-def start(url):
-	"""
-	Function 'start(url)' provides:
-		* page inspection
-		* information about:
-			** login form
-			** login variable
-			** password varialbe
-		* generation of http request pattern
-		* brute force attack ability
-	"""
-	resp = bf.make_empty_get_request(url)
-
-	target = inspect_page(resp.text)
-	target.cookies = resp.cookies
-	target.headers = resp.headers
-	if not target.url:
-		target.url = url
-
-	ask_login_form(target)
-	ask_login_var(target)
-	ask_password_var(target)
-	target.unsuccess_sign = ask_unsuccess_sign()
-	num_threads = int(ask_num_threads())
-	target.generate_request_body()	
-
-	print('Starting brute force attack.')
-	#bf.brute_force(target, logins, passwords, num_threads)
-
-# ================================= END OF COMMANDS ================================= #
+	while param:
+		query += param
+		query += '&'
+		param = input()
 	
+	return query[:-1] # remove last '&' symbol #
 
 
+def ask_request_body():
+	item = input('Set request body: ')
+	body = { }
+	if item.count('=') > 1 and item.count('&') > 0:
+		for i in item.split('&'):
+			key = i.split('=')[0]
+			value = i.split('=')[1]
+			body[key] = value
+	else:
+		while item:
+			key = item.split('=')[0]
+			value = item.split('=')[1]
+			body[key] = value
+			item = input()
+	return body
 
 
-
-
-
-
-
-
-
-	
-
-# ================================= USER INPUT FUNCTIONS ================================= #
-
-# ask user whether he wants to specify login form 
-def ask_login_form(target):
-	print('Guessed login form number is: ', end = ' ')
-	print(target.login_form_id)
-	answer = input('Specify it? (y/n) ').lower()
-	if answer == 'y':
-		target.login_form_id = int(input('New login form number: '))
-		target.set_login_form(target.login_form_id)
-
-# ask user whether he wants to specify login variable
-def ask_login_var(target):
-	print('Guessed login variable is: ' + target.login_var)
-	answer = input('Specify it? (y/n) ').lower()
-	if answer == 'y':
-		target.login_var = input('Login variable: ')
-
-# ask user whether he wants to specify password variable
-def ask_password_var(target):
-	print('Guessed password variable is: ' + target.pass_var)
-	answer = input('Specify it? (y/n) ').lower()
-	if answer == 'y':
-		target.pass_var = input('Password variable: ')
-
+def ask_cookies():
+	print('Set cookies: ')
+	cookies = { }
+	param = input()
+	while param:
+		key = param.split('=')[0]
+		value = param.split('=')[1]
+		cookies[key] = value
+		param = input()
+	return cookies
 
 
 def ask_unsuccess_sign():
@@ -332,27 +209,42 @@ def ask_unsuccess_sign():
 	return input('Set unsuccess sign (look help for more info): ')
 
 
-
-# ask how many threads to use
-def ask_num_threads():
-	print('Default number of threads is: 3')
-	answer = input('Set another number (max = 16)? (y/n) ').lower()
-	if answer == 'y':
-		num = int(input('Threads number: '))
-		if num > 16:
-			print('Error. Max number of threads: 16.')
-			return ask_num_threads()
-		else:
-			return num
-	return 3
-
-# ================================= END OF FUNCTIONS ================================= #
+def ask_login_var():
+	login_var = input('Login variable: ')
+	return login_var
 
 
+def ask_password_var():
+	pass_var = input('Password variable: ')
+	return pass_var
+
+
+
+def generate_request_pattern():
+	"""
+	Generate http request pattern by user input:
+		* set headers
+		* set cookies
+		* set request body
+		* set query string
+	"""
+	req = RequestPattern()
+	req.headers = ask_http_headers()
+	req.query = ask_query_string()
+	# req.cookies = ask_cookies()
+	req.request_body = ask_request_body()
+	req.login_var = ask_login_var()
+	req.pass_var = ask_password_var()
+	req.unsuccess_sign = ask_unsuccess_sign()
+	return req
+
+# ================================= END OF COMMANDS ================================= #
 	
+
 
 ### START PROGRAM ###
 if __name__ == "__main__":
-	main()
+	parser = define_args()
+	args = vars(parser.parse_args())
+	main(args)
 ### PROGRAM FINISHED ###
-
