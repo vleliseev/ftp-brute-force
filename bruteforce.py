cc9fb2e5d6cbbc1e61f5365f820eb3956c3e01e2
Vlad Eliseev <shmel3@openmailbox.org>
2018-07-22 18:55:34 +0300

    fix headers

--- bruteforce.py
+++ bruteforce.py
@@ -25,8 +25,8 @@ def make_request(pattern, login, password):
 	pattern.substitute_login(login)
 	pattern.substitute_password(password)
 	response = requests.request(pattern.http_method, 
-				headers = pattern.headers,
 				pattern.url, 
+			 	headers = pattern.headers,
 				data = pattern.request_body,
 				cookies = pattern.cookies
 				)
