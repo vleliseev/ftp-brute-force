891b50c42fa3d1b6daec6ebe8843e412e844ace5
Null <shmel3@localhost.localdomain>
2018-07-31 23:20:37 +0300

    add config file

---
+++ config.py
@@ -0,0 +1,17 @@
+
+
+### APP HEADERS ###
+__author__ = "Vlad Eliseev"
+__license__ = "GNU GPLv3"
+__email__ = "shmelv3@yandex.ru"
+__status__ = "Development"
+### END HEADERS ###
+
+
+### APP CONSTANTS ###
+LOGIN_INPUT_ERROR = 'Unable to get login input.'
+PASSWORD_INPUT_ERROR = 'Unable to get password input.'
+XPATH_CONTAINS = "//{}[contains({}, '{}')]"
+ATTEMP_ERROR = "Login attemp error."
+LOGIN_ATTEMP = 'Tried {} : {} - {}'
+### END CONSTANTS ###
